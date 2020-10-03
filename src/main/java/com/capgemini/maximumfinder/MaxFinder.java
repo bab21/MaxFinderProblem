9109b26848d9c936528a0d16a2e3ed51513b277c
bab21 <2016061@iiitdmj.ac.in>
2020-10-03 19:16:20 +0530

    Refactoring to implement generic method for all 3 use cases

--- src/main/java/com/capgemini/maximumfinder/MaxFinder.java
+++ src/main/java/com/capgemini/maximumfinder/MaxFinder.java
@@ -2,28 +2,13 @@ package com.capgemini.maximumfinder;
 
 public class MaxFinder {
 	
-	public Integer find_maximum(Integer a,Integer b,Integer c) {
-		Integer max=a;
+	public <E extends Comparable>  E find_maximum(E a,E b,E c) {
+		E max=a;
 		if(b.compareTo(max)>0)
 			max=b;
 		if(c.compareTo(max)>0)
 			max=c;
 		return max;
 	}
-	public Float find_maximum(Float a,Float b,Float c) {
-		Float max=a;
-		if(b.compareTo(max)>0)
-			max=b;
-		if(c.compareTo(max)>0)
-			max=c;
-		return max;
-	}
-	public String find_maximum(String a,String b,String c) {
-		String max=new String(a);
-		if(b.compareTo(max)>0)
-			max=new String(b);
-		if(c.compareTo(max)>0)
-			max=new String(c);
-		return max;
-	}
+
 }
