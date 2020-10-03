9a34fe9f7440c71c68b3e75583f957a92cc57a2b
bab21 <2016061@iiitdmj.ac.in>
2020-10-03 18:42:15 +0530

    use case 2 and its 3 test cases implemented

--- src/main/java/com/capgemini/maximumfinder/MaxFinder.java
+++ src/main/java/com/capgemini/maximumfinder/MaxFinder.java
@@ -10,4 +10,12 @@ public class MaxFinder {
 			max=c;
 		return max;
 	}
+	public Float find_maximum(Float a,Float b,Float c) {
+		Float max=a;
+		if(b.compareTo(max)>0)
+			max=b;
+		if(c.compareTo(max)>0)
+			max=c;
+		return max;
+	}
 }
--- src/test/java/com/capgemini/maximumfinder/MaxFinderTester.java
+++ src/test/java/com/capgemini/maximumfinder/MaxFinderTester.java
@@ -25,5 +25,19 @@ public class MaxFinderTester {
 	public void test1_3() {
 		assertEquals((Integer)30,(Integer)maxfinder.find_maximum(9, 2, 30));
 	}
+	
+	@Test
+	public void test2_1() {
+		assertEquals((Float)9.0F,(Float)maxfinder.find_maximum(9.0F, 2.0F, 3.0F));
+	}
+	@Test
+	public void test2_2() {
+		assertEquals((Float)20.0F,(Float)maxfinder.find_maximum(9.0F, 20.0F, 12.0F));
+	}
+	@Test
+	public void test2_3() {
+		assertEquals((Float)30.0F,(Float)maxfinder.find_maximum(9.0F, 2.0F, 30.0F));
+	}
+	
 
 }
