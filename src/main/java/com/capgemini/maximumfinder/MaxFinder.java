61874a15f01160d481ff62e9d0a4693ba7d0ce0f
bab21 <2016061@iiitdmj.ac.in>
2020-10-04 14:52:49 +0530

    UC4

--- src/main/java/com/capgemini/maximumfinder/MaxFinder.java
+++ src/main/java/com/capgemini/maximumfinder/MaxFinder.java
@@ -13,20 +13,14 @@ public class MaxFinder <E extends Comparable> {
 	
 	public static <E extends Comparable>  E find_maximum(MaxFinder maxfinder){
 		
-		
 		Optional<E> checkNull = Optional.ofNullable(maxfinder.array[maxfinder.array.length-1]);   
         if (checkNull.isPresent()) {
         	Arrays.sort(maxfinder.array);
-        	printMax((E)maxfinder.array[maxfinder.array.length-1]);
     		return (E)maxfinder.array[maxfinder.array.length-1];
         } 
         else{ 
-        	System.out.println("word is null"); 
+        	System.out.println("array is empty"); 
             return null;
        }
 	}
-	public static <E> void printMax (E max) {
-		System.out.println(max);
-	}
-	
 }
